f825b48c1f8aabea129afdc6ff56a581267493e7
Christopher Singer <chris.singer@dmetrics.com>
2024-06-16 13:10:44 -0400

    checkpoint

--- src/dataloader/script.py
+++ src/dataloader/script.py
@@ -1,6 +1,7 @@
 from src.utils import ROOT_DIR
 from typing import Union
 import re
+import json
 from src.dataclass.script import Script
 
 
@@ -9,6 +10,14 @@ class ScriptParser:
         self.script_path = script_path
         self.lines: Script = Script([])
         self.parse()
+
+    def tokenize(self, line) -> list[str]:
+        """
+        This function takes a line of dialogue and returns a list of tokens.
+        """
+        pattern = r"[\w]+|[^\w\s]"
+        tokens = re.findall(pattern, line)
+        return tokens
 
     def combine_lines(self):
         """
@@ -30,6 +39,23 @@ class ScriptParser:
                 combined_lines.append(line)
 
         self.lines = combined_lines
+
+    def sentence_splitter(self) -> None:
+        """
+        This function splits dialogue into individual sentences.
+        """
+
+        split_lines = []
+
+        for line in self.lines:
+            sentences = line["dialogue"].split(".")
+            for sentence in sentences:
+                if sentence != "":
+                    split_lines.append(
+                        {"speaker": line["speaker"], "dialogue": sentence.strip()}
+                    )
+
+        self.lines = split_lines
 
     def tagger(self, line) -> dict[str, str]:
         """
@@ -87,8 +113,27 @@ class ScriptParser:
             self.lines.append(self.tagger(line))
 
         self.combine_lines()
+        # self.sentence_splitter()
+
+    def write_cleaned_script(self, output_path):
+        with open(output_path, "w") as f:
+            for line in self.lines:
+                json.dump(line, f, ensure_ascii=False)
+                f.write("\n")
+
+    def prepare_annotation(self, output_path):
+        with open(output_path, "w") as f:
+            for line in self.lines:
+                # line["gameplay"] = False
+                # line["tokens"] = self.tokenize(line["dialogue"])
+                # line["ner_tags"] = ["O" for _ in line["tokens"]]
+                line["text"] = f"{line['speaker']}: {line['dialogue']}"
+                del line["dialogue"]
+                json.dump(line, f, ensure_ascii=False)
+                f.write("\n")
 
 
 if __name__ == "__main__":
-    parser = ScriptParser(f"{ROOT_DIR}/data/crititcalrole/(2x01)_CuriousBeginnings.txt")
-    print(parser.lines[:5])
+    parser = ScriptParser(f"{ROOT_DIR}/data/raw/(2x01)_CuriousBeginnings.txt")
+    parser.write_cleaned_script(f"{ROOT_DIR}/data/clean/1.jsonl")
+    parser.prepare_annotation(f"{ROOT_DIR}/data/annotated/1.jsonl")
--- src/nlp/test.py
+++ src/nlp/test.py
@@ -1,7 +1,26 @@
 from src.dataloader.script import ScriptParser
 from src.utils import ROOT_DIR
 
+from transformers import AutoTokenizer, AutoModelForTokenClassification
+from transformers import pipeline
 
 data = f"{ROOT_DIR}/data/crititcalrole/(2x01)_CuriousBeginnings.txt"
 parser = ScriptParser(data)
-print(parser.lines[:5])
+
+example = parser.lines[2]["dialogue"]
+
+
+from transformers import AutoTokenizer, AutoModelForTokenClassification
+from transformers import pipeline
+
+tokenizer = AutoTokenizer.from_pretrained("dslim/distilbert-NER")
+model = AutoModelForTokenClassification.from_pretrained("dslim/distilbert-NER")
+
+nlp = pipeline("ner", model=model, tokenizer=tokenizer)
+
+ner_results = nlp(example)
+
+# print meanful results
+for result in ner_results:
+    if result["entity"] != "LABEL_0":
+        print(result)
